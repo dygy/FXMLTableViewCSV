e776ff1250c1ff73802ae11c72d365b0ba5925b2
Arkadii Shwartsman <yukimurachris@gmail.com>
2018-03-21 11:02:43 +0300

    - some error

--- src/fxmltableview/main/java/fxmltableview/Person.java
+++ src/fxmltableview/main/java/fxmltableview/Person.java
@@ -4,17 +4,14 @@ import fxmltableview.main.Records;
 public class Person {
     Records records;
     String firstName = records.getFirstname();
-    Records records;
     String lastName = records.getLastname();
-    Records records;
     String   email = records.getEmail();
-    Records records;
     String status = records.getStatus();
 
 
     public Person(String firstName, String lastName, String email,String status) {
 
-        public Person(String , String , String email, String status) {
+        public Person(String , String , String , String); {
             this(" ", " ", " "," ");
 
      }
